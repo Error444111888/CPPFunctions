395f1da39b190c288fda9b93eb76c2a55c33d641
error418 <error418@hotmail.com>
2018-11-01 03:36:06 -0700

    Initial

---
+++ CPPFunctions/Source.cpp
@@ -0,0 +1,28 @@
+// CPPFunctions - by Ray
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+/*Swap function*/
+
+	inline void swap(int & a, int & b)
+	{
+		int temp = a;
+		a = b;
+		b = temp;
+	}
+
+	
+
+
+
+	cin.get();
+
+}
